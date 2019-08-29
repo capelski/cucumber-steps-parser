5d8f735a2d6300f6aa8f95aa341064bd25a6acae
Carles Capellas <capellas.carles@gmail.com>
2019-08-29 09:43:24 +0200

    Add command line executable

---
+++ src/cli.ts
@@ -0,0 +1,20 @@
+import * as yargs from 'yargs';
+import { getFolderCucumberSentences } from './lib/cucumber-sentences';
+
+const argv = yargs.argv;
+const path: string | undefined = argv._[0];
+
+if (!path) {
+    console.log(`Usage: cucumber-steps-parser <path>
+E.g. cucumber-steps-parser /c/sage/git/etna/@sage/etna-cli`);
+    process.exit(1);
+} else {
+    try {
+        // TODO Support recursive and stepDefinitionRegEx parameters
+        const sentences = getFolderCucumberSentences(path, true);
+        console.log(sentences);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
